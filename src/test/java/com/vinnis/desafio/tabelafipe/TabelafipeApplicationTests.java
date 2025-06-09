a14aad621596ae255ff1eacecaf84e8d0b654987
Vinicius Silva <vinicius4life@gmail.com>
2025-06-09 16:04:28 -0300

    Inicializaçao do projeto

---
+++ src/test/java/com/vinnis/desafio/tabelafipe/TabelafipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinnis.desafio.tabelafipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelafipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
